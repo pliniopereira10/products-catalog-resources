77a52e506596fc7b554f306381719fe9d978d088
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-27 20:29:41 -0300

    If applied, this will be initial commit
    will create backend of the project

---
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package edu.pliniopereira10.dscatlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/pliniopereira10/dscatlog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pliniopereira10.dscatlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
