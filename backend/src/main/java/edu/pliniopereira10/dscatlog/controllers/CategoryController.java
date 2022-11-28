705e445431ec494eaf838caa7975d987ac1d1cfe
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-28 15:13:56 -0300

    If applied, this commit will create the GET/categories/{id} endpoint

--- backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/CategoryController.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,6 +47,13 @@ public class CategoryController {
 		Page<CategoryDTO> pages = service.findAllPagedCategories(request);
 
 		return ResponseEntity.ok().body(pages);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> getOneCategory(@PathVariable Long id) {
+		CategoryDTO dto = service.findCategoryById(id);
+
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
