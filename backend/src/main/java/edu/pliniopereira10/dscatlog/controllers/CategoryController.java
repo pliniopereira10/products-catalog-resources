883962d5d69c818aafd8f753ec4fddc3440ff28b
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-28 08:14:11 -0300

    If applied, this commit will create the CategoryController class
    of the controllers package

---
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/CategoryController.java
@@ -0,0 +1,10 @@
+package edu.pliniopereira10.dscatlog.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+}
