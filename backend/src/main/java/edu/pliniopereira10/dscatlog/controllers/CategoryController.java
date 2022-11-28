59c0ff3228070bc518c01c82556b2f0ecb3a9e0e
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-28 11:56:22 -0300

    If applied, this commit will create GET/categories endpoint
    find categories paged

--- backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/CategoryController.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/CategoryController.java
@@ -3,10 +3,15 @@ package edu.pliniopereira10.dscatlog.controllers;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,8 +30,22 @@ public class CategoryController {
 		dto = service.save(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/[id}")
 				.buildAndExpand(dto.getId()).toUri();
-		
+
 		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<CategoryDTO>> getAllCategories(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+
+		PageRequest request = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Page<CategoryDTO> pages = service.findAllPagedCategories(request);
+
+		return ResponseEntity.ok().body(pages);
 	}
 
 }
