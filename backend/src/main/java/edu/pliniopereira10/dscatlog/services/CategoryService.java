cb2e6b6c180dea7400811414e90b2591da9ac80a
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-28 10:15:04 -0300

    If applied, this commit will create the save method

--- backend/src/main/java/edu/pliniopereira10/dscatlog/services/CategoryService.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/services/CategoryService.java
@@ -1,8 +1,27 @@
 package edu.pliniopereira10.dscatlog.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.pliniopereira10.dscatlog.dtos.CategoryDTO;
+import edu.pliniopereira10.dscatlog.entities.CategoryEntity;
+import edu.pliniopereira10.dscatlog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	@Transactional
+	public CategoryDTO save(CategoryDTO dto) {
+		CategoryEntity entity = new CategoryEntity();
+		entity.setName(dto.getName());
+		
+		entity = repository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
 
 }
