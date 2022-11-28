aa4ab23096bfdfe86078784418a29cc4a1830d6e
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-28 11:52:28 -0300

    If applied, this commit will create the findAllPagedCategories method

--- backend/src/main/java/edu/pliniopereira10/dscatlog/services/CategoryService.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/services/CategoryService.java
@@ -1,6 +1,8 @@
 package edu.pliniopereira10.dscatlog.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,18 +12,25 @@ import edu.pliniopereira10.dscatlog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional
 	public CategoryDTO save(CategoryDTO dto) {
 		CategoryEntity entity = new CategoryEntity();
 		entity.setName(dto.getName());
-		
+
 		entity = repository.save(entity);
-		
+
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPagedCategories(PageRequest request) {
+		Page<CategoryEntity> entities = repository.findAll(request);
+
+		return entities.map(x -> new CategoryDTO(x));
 	}
 
 }
