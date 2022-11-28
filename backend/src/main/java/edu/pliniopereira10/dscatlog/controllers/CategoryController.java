6a2e1dd460f32654b9bd0c01bc014239a6224def
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-28 10:17:44 -0300

    If applied, this commit create POST/categories endpoint

--- backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/CategoryController.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/CategoryController.java
@@ -1,10 +1,32 @@
 package edu.pliniopereira10.dscatlog.controllers;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import edu.pliniopereira10.dscatlog.dtos.CategoryDTO;
+import edu.pliniopereira10.dscatlog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
+
+	@Autowired
+	private CategoryService service;
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO dto) {
+		dto = service.save(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/[id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
 
 }
