d46df7b8afaa60e41a0c791a815c246217e05c7b
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-29 18:53:02 -0300

    If applied, this commit will update database seeding

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category (name) VALUES ('Livros');
-INSERT INTO tb_category (name) VALUES ('Eletrônicos');
-INSERT INTO tb_category (name) VALUES ('Computadores');
+INSERT INTO tb_category (name, created_At) VALUES ('Livros', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Eletrônicos', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Computadores', NOW());
