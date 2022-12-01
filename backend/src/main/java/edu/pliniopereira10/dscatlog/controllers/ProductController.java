7c95a346c04ca7d45f36282fc85948107fab6fff
pliniopereira10 <plinio.pereira10@gmail.com>
2022-12-01 18:52:11 -0300

    If applied, this commit will create the DELETE/products/{id} endpoint
    in package controllers.ProductController

--- backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/ProductController.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,5 +64,12 @@ public class ProductController {
 		dto = service.update(id, dto);
 
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deletProduct(@PathVariable Long id) {
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
 	}
 }
