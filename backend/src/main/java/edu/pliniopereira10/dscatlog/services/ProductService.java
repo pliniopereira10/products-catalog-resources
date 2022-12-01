abd43371da283e161e342e75f63b0bef1b2af034
pliniopereira10 <plinio.pereira10@gmail.com>
2022-12-01 18:21:56 -0300

    If applied, this commit will create the save method
    in package services.ProductService

--- backend/src/main/java/edu/pliniopereira10/dscatlog/services/ProductService.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/services/ProductService.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import edu.pliniopereira10.dscatlog.dtos.CategoryDTO;
 import edu.pliniopereira10.dscatlog.dtos.ProductDTO;
+import edu.pliniopereira10.dscatlog.entities.CategoryEntity;
 import edu.pliniopereira10.dscatlog.entities.ProductEntity;
 import edu.pliniopereira10.dscatlog.exceptions.ResourceNotFoundException;
+import edu.pliniopereira10.dscatlog.repositories.CategoryRepository;
 import edu.pliniopereira10.dscatlog.repositories.ProductRepository;
 
 @Service
@@ -18,6 +21,18 @@ public class ProductService {
 
 	@Autowired
 	ProductRepository repository;
+
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	@Transactional
+	public ProductDTO save(ProductDTO dto) {
+		ProductEntity entity = new ProductEntity();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+
+		return new ProductDTO(entity);
+	}
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAll(PageRequest request) {
@@ -33,5 +48,20 @@ public class ProductService {
 		ProductEntity entity = obj.orElseThrow(() -> new ResourceNotFoundException("Produto não encontrado"));
 
 		return new ProductDTO(entity, entity.getCategories());
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, ProductEntity entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setMoment(dto.getMoment());
+
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			CategoryEntity categoryEntity = categoryRepository.getReferenceById(catDto.getId());
+			entity.getCategories().add(categoryEntity);
+		}
+
 	}
 }
