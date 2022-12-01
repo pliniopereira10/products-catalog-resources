6ddc5f1d5f21b4f70aac633c13fc1a43b800aeed
pliniopereira10 <plinio.pereira10@gmail.com>
2022-12-01 18:23:31 -0300

    If applied, this commit will create the POST/products endpoint
    in package controllers.ProductController

--- backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/ProductController.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package edu.pliniopereira10.dscatlog.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,9 +9,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import edu.pliniopereira10.dscatlog.dtos.ProductDTO;
 import edu.pliniopereira10.dscatlog.services.ProductService;
@@ -21,6 +26,16 @@ public class ProductController {
 	@Autowired
 	ProductService service;
 
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> saveProduct(@RequestBody ProductDTO dto){
+		dto = service.save(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/[id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> getAllProducts(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
