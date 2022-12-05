e5b90323359e63d59479b380bbfb96d02524cf10
pliniopereira10 <plinio.pereira10@gmail.com>
2022-12-05 16:36:05 -0300

    If applied, this commit will refactor the GET/products endpoint

--- backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/ProductController.java
+++ backend/src/main/java/edu/pliniopereira10/dscatlog/controllers/ProductController.java
@@ -4,8 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -38,15 +36,8 @@ public class ProductController {
 	}
 
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> getAllProducts(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
-
-		PageRequest request = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-
-		Page<ProductDTO> pages = service.findAll(request);
+	public ResponseEntity<Page<ProductDTO>> getAllProducts(Pageable pageable) {
+		Page<ProductDTO> pages = service.findAll(pageable);
 
 		return ResponseEntity.ok().body(pages);
 	}
@@ -65,7 +56,7 @@ public class ProductController {
 
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> deletProduct(@PathVariable Long id) {
 		service.delete(id);
